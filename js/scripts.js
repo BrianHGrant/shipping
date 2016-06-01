a1f36a20492c824da76980587a16b29538dd1fca
Brian Grant and Tony Fabery <brian@>
2016-06-01 11:23:17 -0700

    add front end code to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 // Back end code
 var total = function(price, tax) {
-  return price * tax;
-}
+  return parseInt(price) * tax;
+};
 
 // Front end code
+$(function() {
+  $("#blank form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    var addressInput = $("input#address").val();
+    var orderdateInput = $("input#orderdate").val();
+    var shippingdateInput = $("input#shippingdate").val();
+    var priceInput = $("input#price").val();
+    var totalprice = total(priceInput, 1.78);
+
+    $(".name").text(nameInput);
+    $(".address").text(addressInput);
+    $(".orderdate").text(orderdateInput);
+    $(".shippingdate").text(shippingdateInput);
+    $(".price").text(priceInput);
+    $(".total").text(totalprice);
+
+    $("#receipt").show();
+    event.preventDefault();
+  });
+});
